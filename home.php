6bdf95cae3181107b04bbe5c48ec79fcae1c3400
unknown <erojoseph94@gmail.com>
2020-06-20 12:13:35 +0100

    First push

---
+++ home.php
@@ -0,0 +1,116 @@
+<html lang="en">
+
+<head>
+    <title>MyRecharge</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/mdb.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <script src="assets/main.js"></script>
+</head>
+
+<body>
+
+    <header>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <strong>MyRecharge</strong>
+                </a>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Github</a>
+                </li>
+            </div>
+            </div>
+        </nav>
+        <!-- Navbar -->
+        <!-- Full Page Intro -->
+        <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Others/images/91.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-gradient align-items-center">
+                <!-- Content -->
+                <div class="container">
+                    <!--Grid row-->
+                    <div class="row mt-5 justify-content-center">
+                        <!--Grid column-->
+                        <!-- <div class="col-md-6 mb-5 mt-md-0 mt-5 white-text text-center text-md-left">
+                            <h1 class="h1-responsive font-weight-bold wow fadeInLeft" data-wow-delay="0.3s">Sign up right now! </h1>
+                            <hr class="hr-light wow fadeInLeft" data-wow-delay="0.3s">
+                            <h6 class="mb-3 wow fadeInLeft" data-wow-delay="0.3s">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga
+                                nesciunt dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea
+                                dolor molestiae, quisquam iste, maiores. Nulla.</h6>
+                            <a class="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Learn more</a>
+                        </div> -->
+                        <!--Grid column-->
+                        <!--Grid column-->
+                        <div class="col-md-6 col-xl-5 mb-4">
+                            <!--Form-->
+                            <div class="card wow fadeInRight" data-wow-delay="0.3s">
+                                <div class="card-body">
+                                    <!--Header-->
+                                    <div class="text-center">
+                                        <h3 class="white-text">
+                                            <i class="fas fa-user white-text"></i> Send Airtime:</h3>
+                                        <hr class="hr-light">
+                                    </div>
+                                    <!--Body-->
+                                    <div class="md-form">
+                                        <i class="fas fa-user prefix white-text active"></i>
+                                        <input type="text" id="form3" class="white-text form-control">
+                                        <label for="form3" class="active">Network Provider</label>
+                                    </div>
+
+                                    <div class="md-form">
+                                        <i class="fas fa-phone prefix white-text active"></i>
+                                        <input type="number" id="form2" class="white-text form-control">
+                                        <label for="form2" class="active">Recepient Phone </label>
+                                    </div>
+                                  
+                                    <div class="md-form">
+                                        <i class="fas fa-money prefix white-text active"></i>
+                                        <input type="number" id="form2" class="white-text form-control">
+                                        <label for="form2" class="active">Amount </label>
+                                    </div>
+                                   
+                                    <div class="text-center mt-4">
+                                        <button class="btn btn-indigo">Send</button>
+                                        <hr class="hr-light mb-3 mt-4">
+                                        <div class="inline-ul text-center">
+                                            <a class="p-2 m-2 tw-ic">
+                                                <i class="fab fa-twitter white-text"></i>
+                                            </a>
+                                            <a class="p-2 m-2 li-ic">
+                                                <i class="fab fa-linkedin-in white-text"> </i>
+                                            </a>
+                                            <a class="p-2 m-2 ins-ic">
+                                                <i class="fas fa-instagram white-text"> </i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--/.Form-->
+                        </div>
+                        <!--Grid column-->
+                    </div>
+                    <!--Grid row-->
+                </div>
+                <!-- Content -->
+            </div>
+            <!-- Mask & flexbox options-->
+        </div>
+        <!-- Full Page Intro -->
+    </header>
+    <!-- Main navigation -->
+    <!--Main Layout-->
+
+    <!--Main Layout-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="assets/mdb.min.js"></script>
+    <script src="assets/main.js"></script>
+</body>
+
+</html>
